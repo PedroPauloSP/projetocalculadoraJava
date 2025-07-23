3a8c4d2e5955a10d3a42e0adc7719a01f5f3b95e
PedroPauloSP <pedropsantospereira@gmail.com>
2025-07-22 21:58:08 -0300

    Criando projeto

--- testeVersionamento/src/main/java/br/com/senac/testeversionamento/TesteVersionamento.java
+++ testeVersionamento/src/main/java/br/com/senac/testeversionamento/TesteVersionamento.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package br.com.senac.testeversionamento;
 
@@ -12,6 +8,6 @@ package br.com.senac.testeversionamento;
 public class TesteVersionamento {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Novo projeto main");
     }
 }
