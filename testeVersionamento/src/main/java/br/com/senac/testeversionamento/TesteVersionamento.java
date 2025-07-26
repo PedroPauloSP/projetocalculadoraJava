71f8224e5c5f3d838dbba10279e574711fb7326d
PedroPauloSP <pedropsantospereira@gmail.com>
2025-07-26 20:12:16 -0300

    Novo teste

--- testeVersionamento/src/main/java/br/com/senac/testeversionamento/TesteVersionamento.java
+++ testeVersionamento/src/main/java/br/com/senac/testeversionamento/TesteVersionamento.java
@@ -1,13 +1,12 @@
 
+
 package br.com.senac.testeversionamento;
 
-/**
- *
- * @author pedro
- */
+
 public class TesteVersionamento {
 
     public static void main(String[] args) {
         System.out.println("Novo projeto main");
+        System.out.println("Criando Projeto");
     }
 }
